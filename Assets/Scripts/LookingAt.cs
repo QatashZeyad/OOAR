0e281f6342352cc93dd2d01bca9b8a25b2eb75db
Andrew Wetmore <Alwaygetgo@gmail.com>
2018-04-11 18:24:37 -0400

    Added print to ui for what you are looking at

---
+++ Assets/Scripts/LookingAt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LookingAt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = "Looking at: "+GetLookAt().name;
+    }
+
+    // Get what the camera is looking at
+    public GameObject GetLookAt()
+    {
+        RaycastHit hit;
+        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit);
+        print(hit.collider.gameObject.name);
+        return hit.collider.gameObject;
+    }
+}
