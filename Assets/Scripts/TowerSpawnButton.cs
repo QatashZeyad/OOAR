0dc0d1f60fd8f6ad3f46df6c7420049ea83d1fe4
dbdunham <dbdunham@email.uscb.edu>
2018-04-12 14:01:32 -0400

    so andrew we need a eventsystem......

--- Assets/Scripts/TowerSpawnButton.cs
+++ Assets/Scripts/TowerSpawnButton.cs
@@ -6,20 +6,33 @@ using UnityEngine.UI;
 public class TowerSpawnButton : MonoBehaviour {
 
     // The tower to spawn
-    public GameObject tower;
+    //public GameObject tower;
+	public GameObject menuPanel;
+
+	private bool menuOut = false;
 
     // Adds the listener for the button
     void Start()
     {
-        this.GetComponent<Button>().onClick.AddListener(CreateSingleTargetTower);
+		print ("Hi");
+		this.GetComponent<Button>().onClick.AddListener(LoadNewPanel);
     }
     
     // Creates a single target tower on click
-    void CreateSingleTargetTower()
+    /*void CreateSingleTargetTower()
     {
-        GameObject spawned = Instantiate(tower, Level.GetLookAt().transform.position, Quaternion.Euler(0, 0, 0));
+		GameObject tile = Level.GetLookAt ();
+		GameObject spawned = Instantiate(tower, tile.transform.position, tile.transform.rotation);
         spawned.transform.SetParent(GameObject.FindGameObjectWithTag("Map").transform);
-    }
+    }*/
 
-
+	void LoadNewPanel()
+	{
+		print ("Hi2");
+		if (menuOut)
+			menuPanel.transform.position += new Vector3(240, 0, 0);
+		else
+			menuPanel.transform.position += new Vector3(-240, 0, 0);
+		menuOut = !menuOut;
+	}
 }
